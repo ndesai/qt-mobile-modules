18281ff17a090430899481c06eb5df814ce7b9fb
Niraj Desai <niraj@app.st>
2014-06-22 12:24:24 -0700

    Removed extraneous code

--- Platform/platformios.mm
+++ Platform/platformios.mm
@@ -20,24 +20,6 @@
     return self;
 }
 
-//- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
-//{
-//    // Create the path where we want to save the image:
-//    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-//    path = [path stringByAppendingString:@"/capture.png"];
-
-//    // Save image:
-//    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
-//    [UIImagePNGRepresentation(image) writeToFile:path options:NSAtomicWrite error:nil];
-
-//    // Update imagePath property to trigger QML code:
-//    m_iosCamera->m_imagePath = QStringLiteral("file:") + QString::fromNSString(path);
-//    emit m_iosCamera->imagePathChanged();
-
-//    // Bring back Qt's view controller:
-//    [picker dismissViewControllerAnimated:YES completion:nil];
-//}
-
 @end
 
 
@@ -87,24 +69,6 @@ void PlatformiOS::setApplicationIconBadgeNumber(int arg)
         emit applicationIconBadgeNumberChanged(arg);
     }
 }
-
-//void IOSCamera::open()
-//{
-//    // Get the UIView that backs our QQuickWindow:
-//    UIView *view = static_cast<UIView *>(
-//                QGuiApplication::platformNativeInterface()
-//                ->nativeResourceForWindow("uiview", window()));
-//    UIViewController *qtController = [[view window] rootViewController];
-
-//    // Create a new image picker controller to show on top of Qt's view controller:
-//    UIImagePickerController *imageController = [[[UIImagePickerController alloc] init] autorelease];
-//    [imageController setSourceType:UIImagePickerControllerSourceTypeCamera];
-//    [imageController setDelegate:id(m_delegate)];
-
-//    // Tell the imagecontroller to animate on top:
-//    [qtController presentViewController:imageController animated:YES completion:nil];
-//}
-
 
 void PlatformiOS::setStatusBarVisible(bool arg)
 {
